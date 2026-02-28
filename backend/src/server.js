e4517fd27963e712a9505de198bf4c9553d6a0b3
Sakshi Saxena <155166160+Sakshi-2023@users.noreply.github.com>
2026-02-28 22:06:52 +0530

    Add files via upload

---
+++ backend/src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRoutes);
+// Test Route
+app.get("/", (req, res) => {
+  res.json({ message: "Server is running 🚀" });
+});
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected ✅");
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB Connection Error ❌", err);
+  });
